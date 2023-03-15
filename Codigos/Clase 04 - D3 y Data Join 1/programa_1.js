7cad81a1671eba52d8e2f76ff8251401170eba97
Hernán Valdivieso <hfvaldivieso@uc.cl>
2023-03-15 19:36:48 -0300

    clase 4

---
+++ Codigos/Clase 04 - D3 y Data Join 1/programa_1.js
@@ -0,0 +1,7 @@
+const selection = d3.selectAll("div");
+
+selection.append("h1").style("color", "orange").text("uwu");
+
+selection.append("h1").text("uwucito");
+
+// d3.selectAll("div").selectAll("h1").text((d, i) => i) // ¿qué ocurre al ejecutar esto?
