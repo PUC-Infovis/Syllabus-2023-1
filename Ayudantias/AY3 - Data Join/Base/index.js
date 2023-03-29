64d85af02364454f4a8c778215e6eda9bd4f7c6b
Enzo Morata <enzo.morata@uc.cl>
2023-03-29 09:16:45 -0300

    add ayudantia 3

---
+++ Ayudantias/AY3 - Data Join/Base/index.js
@@ -0,0 +1,19 @@
+// 0) Espacio para definir nuestras constantes, contenedores y referencias a SVG
+
+
+// 2) Definir una función para manejar los datos
+
+
+// 3) Definir la función para generar la visualización
+
+
+// 1) Lectura y Parseo de datos
+
+
+
+
+
+
+
+
+
